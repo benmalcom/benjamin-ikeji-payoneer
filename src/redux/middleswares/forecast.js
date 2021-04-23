4498c182ae7b140a585b3283c5926a2a071e243b
Ikeji Benjamin <benjaminikeji@gmail.com>
2021-04-23 13:56:32 +0100

    add redux(reducers, store and middlewares)

---
+++ src/redux/middleswares/forecast.js
@@ -0,0 +1,64 @@
+import {
+  apiRequest,
+  APPLY_METRICS_TO_FORECASTS,
+  FETCH_FORECASTS,
+  applyMetricToForecasts as applyMetrics,
+  updateForecastList,
+  setCurrentMetric,
+} from '../actions';
+import { requestKeys } from '../../services/_request/keys';
+import { celsiusToFahrenheit, fahrenheitToCelcius, metricValues } from '../../utils/forecast';
+
+const fetchForecasts = ({ dispatch, getState }) => next => action => {
+  next(action);
+  if (action.type === FETCH_FORECASTS.START) {
+    const { ui: { pagination } } = getState();
+    const { key = requestKeys.fetchForecasts, params, ...rest } = action.meta;
+
+    const defaultPagination = pagination.default;
+
+    dispatch(
+      apiRequest({
+        url: '/forecast',
+        key: key,
+        params: {
+          ...params,
+          ...defaultPagination,
+        },
+        onSuccess: data => {
+          dispatch({ type: FETCH_FORECASTS.SUCCESS, payload: data?.list || [] });
+        },
+        ...rest,
+      }),
+    );
+  }
+};
+
+const applyMetricToForecasts = ({ dispatch, getState }) => next => action => {
+  next(action);
+  if (action.type === APPLY_METRICS_TO_FORECASTS) {
+    const { metric } = action.meta;
+    const { forecasts: { byList } } = getState();
+    if (!metric || !byList?.length)
+      return;
+    const payload = byList.map(item => {
+      const isToCelcius = metricValues.CELCIUS === metric;
+      const metricFunc = isToCelcius ? fahrenheitToCelcius : celsiusToFahrenheit;
+
+      return {
+        ...item,
+        main: {
+          ...item.main,
+          temp: metricFunc(item.main?.temp),
+          feels_like: metricFunc(item.main?.feels_like),
+          temp_min: metricFunc(item.main?.temp_min),
+          temp_max: metricFunc(item.main?.temp_max),
+        },
+      };
+    });
+    dispatch(updateForecastList(payload));
+    dispatch(setCurrentMetric(metric));
+  }
+};
+
+export default [fetchForecasts, applyMetricToForecasts];
---
+++ src/redux/reducers/forecast.js
@@ -0,0 +1,51 @@
+import { groupBy, last } from 'lodash';
+import dayjs from 'dayjs';
+import {
+  FETCH_FORECASTS,
+  RESET_FORECAST_STATE, SET_CURRENT_METRIC, UPDATE_FORECAST_LIST,
+} from '../actions';
+import { metricValues } from '../../utils/forecast';
+
+const initialState = {
+  currentMetric: metricValues.CELCIUS,
+  byList: [],
+  byDate: {},
+  forNextDays: [],
+};
+
+const forecastReducer = (state = initialState, action) => {
+  const { payload, type } = action;
+  switch (type) {
+    case UPDATE_FORECAST_LIST:
+    case FETCH_FORECASTS.SUCCESS: {
+      const byList = payload;
+      const byDate = groupBy(byList, item => dayjs(item.dt_txt).format('DD/MM/YYYY'));
+      const forNextDays = Object.values(byDate).map(groupList => {
+        const item = {...last(groupList)};
+        const sum = groupList.reduce( ( a, c ) => a + c?.main?.temp, 0 )
+        return {
+          ...item,
+          avg_temp: sum / groupList.length
+        }
+      });
+
+      return Object.assign({}, state, {
+        byList,
+        byDate,
+        forNextDays,
+      });
+    }
+    case SET_CURRENT_METRIC:
+      return {
+        ...state,
+        currentMetric: payload,
+      };
+    case RESET_FORECAST_STATE:
+      return initialState;
+    default:
+      return state;
+  }
+};
+
+export default forecastReducer;
+
