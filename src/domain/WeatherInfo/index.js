4ca6f9dbdfa605e122eeefb8aea5ac51a8ae9776
Ikeji Benjamin <benjaminikeji@gmail.com>
2021-04-23 19:02:31 +0100

    Update main page view

---
+++ src/domain/WeatherInfo/index.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Container } from '@material-ui/core';
+import dayjs from 'dayjs';
+import { fetchForecasts, resetForecastState, applyMetricToForecasts, setCurrentMetric } from 'redux/actions';
+import { requestKeys } from 'services/_request/keys';
+import Carousel from 'components/Weather/Carousel';
+import MetricSwitcher from 'components/Weather/MetricSwitcher';
+import Loading from 'components/UI/Loading';
+import DailyWeatherBarChart from 'components/Weather/DailyWeatherChart';
+import usePrevious from 'utils/hooks/use-previous'
+
+const Forecast = () => {
+  const [currentDate, setCurrentDate] = useState();
+  const dispatch = useDispatch();
+  const { forecastsForNextDays, loading, currentMetric, forecastsByDate } = useSelector(({ forecasts, ui }) => ({
+    forecastsForNextDays: forecasts.forNextDays,
+    forecastsByDate: forecasts.byDate,
+    currentMetric: forecasts.currentMetric,
+    loading: ui.loading[requestKeys.fetchForecasts],
+  }));
+
+  const getForecasts = (params) => dispatch(fetchForecasts(params));
+
+  useEffect(() => {
+    getForecasts({ q: 'Lagos', units: 'metric' });
+    return () => dispatch(resetForecastState());
+  }, []);
+
+  useEffect(() => {
+    if (forecastsForNextDays?.length)
+      dispatch(applyMetricToForecasts(currentMetric));
+    if (previousCurrentDate)
+      setCurrentDate(previousCurrentDate);
+  }, [currentMetric]);
+
+
+  useEffect(() => {
+    if (forecastsForNextDays?.length) {
+      const todayForecast = forecastsForNextDays[0];
+      const date = previousCurrentDate || dayjs(todayForecast.dt_txt).format('DD/MM/YYYY');
+      setCurrentDate(date);
+    }
+  }, [forecastsForNextDays]);
+
+  const previousCurrentDate = usePrevious(currentDate);
+
+  const onMetricChange = e => dispatch(setCurrentMetric(e.target.value));
+
+  const onCardItemClick = date => setCurrentDate(date);
+
+  const forecastsForSelectedDate = forecastsByDate[currentDate];
+
+  return loading ? <Loading /> : <Container>
+    <MetricSwitcher value={currentMetric} onChange={onMetricChange} />
+    <Carousel
+      metric={currentMetric}
+      selectedDate={currentDate}
+      items={forecastsForNextDays}
+      onCardItemClick={onCardItemClick} />
+    {forecastsForSelectedDate && <DailyWeatherBarChart metric={currentMetric}
+                                                       data={forecastsForSelectedDate} />}
+  </Container>;
+
+};
+
+export default Forecast;
