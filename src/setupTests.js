083969c9f48444bbf6478beefa1269c3312b9a5a
Ikeji Benjamin <benjaminikeji@gmail.com>
2021-04-23 22:48:53 +0100

    Update eslint fix

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,4 @@
-import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { cleanup } from '@testing-library/react';
 
 window.matchMedia = window.matchMedia || function() {
   return {
