a4180ad6afbc4fdd1de8d6a0ced90f66e7473b0c
Benjamin Ikeji <benjaminikeji@gmail.com>
2021-05-20 13:03:18 +0100

    Fix user story 2

--- src/domain/WeatherInfo/index.js
+++ src/domain/WeatherInfo/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Container } from '@material-ui/core';
+import { Button, Container, Box } from '@material-ui/core';
 import dayjs from 'dayjs';
 import { fetchForecasts, resetForecastState, applyMetricToForecasts, setCurrentMetric } from 'redux/actions';
 import { requestKeys } from 'services/_request/keys';
@@ -8,7 +8,10 @@ import ForecastCarousel from 'components/Weather/ForecastCarousel';
 import MetricSwitcher from 'components/Weather/MetricSwitcher';
 import Loading from 'components/UI/Loading';
 import DailyWeatherBarChart from 'components/Weather/DailyWeatherChart';
-import usePrevious from 'utils/hooks/use-previous'
+import usePrevious from 'utils/hooks/use-previous';
+import styles from './WeatherInfo.module.scss';
+
+const PARAMS = { q: 'Munich,de', units: 'metric' };
 
 const Forecast = () => {
   const [currentDate, setCurrentDate] = useState();
@@ -23,19 +26,16 @@ const Forecast = () => {
   const getForecasts = (params) => dispatch(fetchForecasts(params));
 
   useEffect(() => {
-    getForecasts({ q: 'Munich,de', units: 'metric' });
+    getForecasts(PARAMS);
     return () => {
       dispatch(resetForecastState());
-    }
+    };
   }, []);
 
   useEffect(() => {
-    if (forecastsForNextDays?.length)
-      dispatch(applyMetricToForecasts(currentMetric));
-    if (previousCurrentDate)
-      setCurrentDate(previousCurrentDate);
+    if (forecastsForNextDays?.length) dispatch(applyMetricToForecasts(currentMetric));
+    if (previousCurrentDate) setCurrentDate(previousCurrentDate);
   }, [currentMetric]);
-
 
   useEffect(() => {
     if (forecastsForNextDays?.length) {
@@ -47,25 +47,38 @@ const Forecast = () => {
 
   const previousCurrentDate = usePrevious(currentDate);
 
-  const onMetricChange = e => dispatch(setCurrentMetric(e.target.value));
+  const onMetricChange = (e) => dispatch(setCurrentMetric(e.target.value));
 
-  const onCardItemClick = date => setCurrentDate(date);
+  const onCardItemClick = (date) => setCurrentDate(date);
 
   const forecastsForSelectedDate = forecastsByDate[currentDate];
 
-  return loading ? <Loading /> : <Container>
-    <MetricSwitcher value={currentMetric} onChange={onMetricChange} />
-    <ForecastCarousel
-      currentMetric={currentMetric}
-      setCurrentDate={setCurrentDate}
-      metric={currentMetric}
-      selectedDate={currentDate}
-      items={forecastsForNextDays}
-      onCardItemClick={onCardItemClick} />
-    {forecastsForSelectedDate && <DailyWeatherBarChart metric={currentMetric}
-                                                       data={forecastsForSelectedDate} />}
-  </Container>;
-
+  return loading ? (
+    <Loading />
+  ) : (
+    <Container className={styles.container}>
+      <Box className={styles['action-bar']}>
+        <MetricSwitcher value={currentMetric} onChange={onMetricChange} />
+        <Button
+          color="primary"
+          variant="contained"
+          onClick={() => getForecasts(PARAMS)}
+          className={styles['refresh-btn']}
+        >
+          Refresh
+        </Button>
+      </Box>
+      <ForecastCarousel
+        currentMetric={currentMetric}
+        setCurrentDate={setCurrentDate}
+        metric={currentMetric}
+        selectedDate={currentDate}
+        items={forecastsForNextDays}
+        onCardItemClick={onCardItemClick}
+      />
+      {forecastsForSelectedDate && <DailyWeatherBarChart metric={currentMetric} data={forecastsForSelectedDate} />}
+    </Container>
+  );
 };
 
 export default Forecast;
