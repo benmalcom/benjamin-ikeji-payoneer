5d70bbef3412e7adf9e32fb5cf15c23617221e35
Benjamin Ikeji <benjaminikeji@gmail.com>
2021-08-12 13:08:43 +0100

    Add sdk for appcues

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,37 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Route, Switch, useHistory } from 'react-router-dom';
 
 import Forecast from 'domain/WeatherInfo';
 
-const App = () => (<div>
+const App = () => {
+  const history = useHistory();
+
+  useEffect(() => {
+    if (typeof window.Appcues !== 'undefined') {
+      window.Appcues.identify(Math.random(), {
+        created_at: Date.now(),
+        user: `user_${Math.random() * 10}`,
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!history) return;
+    return history.listen((location) => {
+      console.log(`You changed the page to: ${location.pathname}`);
+      if (typeof window.Appcues !== 'undefined') {
+        window.Appcues.page();
+      }
+    });
+  }, [history]);
+
+  return (
     <BrowserRouter>
       <Switch>
         <Route path="/" component={Forecast} />
       </Switch>
     </BrowserRouter>
-  </div>
-);
+  );
+};
 
 export default App;
